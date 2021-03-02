a9a680ccfa9d85402d3a38455999ad7bea200df3
Brian Chan <brian.chan@liferay.com>
2021-03-02 08:10:49 -0300

    LRDOCS-8875 match package

--- docs/dxp/7.x/en/liferay-internals/extending-liferay/portlet-filters/using-a-portlet-filter/resources/liferay-b4k8.zip/b4k8-api/src/main/java/com/acme/b4k8/constants/B4K8PortletKeys.java
+++ docs/dxp/7.x/en/liferay-internals/extending-liferay/portlet-filters/using-a-portlet-filter/resources/liferay-b4k8.zip/b4k8-api/src/main/java/com/acme/b4k8/constants/B4K8PortletKeys.java
@@ -2,6 +2,7 @@ package com.acme.b4k8.constants;
 
 public class B4K8PortletKeys {
 
-	public static final String B4K8 = "com_acme_b4k8_web_portlet_B4K8Portlet";
+	public static final String B4K8 =
+		"com_acme_b4k8_web_internal_portlet_B4K8Portlet";
 
 }
