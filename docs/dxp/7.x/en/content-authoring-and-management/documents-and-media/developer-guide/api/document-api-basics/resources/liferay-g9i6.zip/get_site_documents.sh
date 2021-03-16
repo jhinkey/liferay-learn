f14233a4db63b748ca55c4340ead0ab58b686855
jhinkey <james.hinkey@liferay.com>
2021-03-16 11:38:28 -0400

    LRDOCS-9156 Remove unneeded options

--- docs/dxp/7.x/en/content-authoring-and-management/documents-and-media/developer-guide/api/document-api-basics/resources/liferay-g9i6.zip/get_site_documents.sh
+++ docs/dxp/7.x/en/content-authoring-and-management/documents-and-media/developer-guide/api/document-api-basics/resources/liferay-g9i6.zip/get_site_documents.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-curl -X GET "http://localhost:8080/o/headless-delivery/v1.0/sites/${1}/documents" -u 'test@liferay.com:test' -H  "accept: application/json" -H  "x-csrf-token: Uo5IStBc"
+curl -X GET "http://localhost:8080/o/headless-delivery/v1.0/sites/${1}/documents" -u 'test@liferay.com:test' -H "accept: application/json" -H "x-csrf-token: Uo5IStBc"
