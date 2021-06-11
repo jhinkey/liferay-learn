1a7437807834d061a95551bf609360db28a15ef6
jhinkey <james.hinkey@liferay.com>
2021-06-11 16:49:35 -0400

    LRDOCS-9512 add commented dest config setters and log dest config settings.

--- docs/dxp/latest/en/developing-applications/core-frameworks/message-bus/tuning-messaging-performance/resources/liferay-w3r2.zip/w3r2-able-impl/src/main/java/com/acme/w3r2/able/internal/messaging/W3R2AbleMessagingConfigurator.java
+++ docs/dxp/latest/en/developing-applications/core-frameworks/message-bus/tuning-messaging-performance/resources/liferay-w3r2.zip/w3r2-able-impl/src/main/java/com/acme/w3r2/able/internal/messaging/W3R2AbleMessagingConfigurator.java
@@ -1,5 +1,8 @@
 package com.acme.w3r2.able.internal.messaging;
 
+import com.liferay.petra.string.StringBundler;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.messaging.Destination;
 import com.liferay.portal.kernel.messaging.DestinationConfiguration;
 import com.liferay.portal.kernel.messaging.DestinationFactory;
@@ -17,14 +20,36 @@ public class W3R2AbleMessagingConfigurator {
 
 	@Activate
 	private void _activate(BundleContext bundleContext) {
-		Destination destination = _destinationFactory.createDestination(
+		DestinationConfiguration destinationConfiguration =
 			DestinationConfiguration.createParallelDestinationConfiguration(
-				"acme/w3r2_able"));
+				"acme/w3r2_able");
+
+		// destinationConfiguration.setMaximumQueueSize(maximumQueueSize);
+		// destinationConfiguration.setWorkersCoreSize(workersCoreSize);
+		// destinationConfiguration.setWorkersMaxSize(workersMaxSize);
+
+		Destination destination = _destinationFactory.createDestination(
+			destinationConfiguration);
 
 		_serviceRegistration = bundleContext.registerService(
 			Destination.class, destination,
 			MapUtil.singletonDictionary(
 				"destination.name", destination.getName()));
+
+		if (_log.isInfoEnabled()) {
+			StringBundler sb = new StringBundler(8);
+
+			sb.append("Destination ");
+			sb.append(destination.getName());
+			sb.append(", max queue size ");
+			sb.append(destinationConfiguration.getMaximumQueueSize());
+			sb.append(", worker core size ");
+			sb.append(destinationConfiguration.getWorkersCoreSize());
+			sb.append(", worker max size ");
+			sb.append(destinationConfiguration.getWorkersMaxSize());
+
+			_log.info(sb.toString());
+		}
 	}
 
 	@Deactivate
@@ -33,6 +58,9 @@ public class W3R2AbleMessagingConfigurator {
 			_serviceRegistration.unregister();
 		}
 	}
+
+	private static final Log _log = LogFactoryUtil.getLog(
+		W3R2AbleMessagingConfigurator.class);
 
 	@Reference
 	private DestinationFactory _destinationFactory;
