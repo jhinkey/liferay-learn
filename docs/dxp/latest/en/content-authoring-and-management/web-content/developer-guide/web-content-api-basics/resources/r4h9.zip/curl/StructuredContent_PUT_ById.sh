0cf83d312c8b374bfa2f518ec27b6f78b5d764ae
Jorge Campo <62282406+jorge-campo@users.noreply.github.com>
2021-08-04 15:25:37 +0200

    LRDOCS-9606 StructuredContents updated cURL scripts (second update)

--- docs/dxp/latest/en/content-authoring-and-management/web-content/developer-guide/web-content-api-basics/resources/r4h9.zip/curl/StructuredContent_PUT_ById.sh
+++ docs/dxp/latest/en/content-authoring-and-management/web-content/developer-guide/web-content-api-basics/resources/r4h9.zip/curl/StructuredContent_PUT_ById.sh
@@ -2,5 +2,5 @@ curl \
 	-H "Content-Type: application/json" \
 	-X PUT \
 	"http://localhost:8080/o/headless-delivery/v1.0/structured-contents/${1}" \
-	-d "{\"contentStructureId\": \"${2}\", \"title\": \"Updated Web Content article\"}" \
+	-d "{\"contentStructureId\": \"${2}\", \"title\": \"Goo\", \"friendlyUrlPath\": \"goo-url\"}" \
 	-u "test@liferay.com:test"
