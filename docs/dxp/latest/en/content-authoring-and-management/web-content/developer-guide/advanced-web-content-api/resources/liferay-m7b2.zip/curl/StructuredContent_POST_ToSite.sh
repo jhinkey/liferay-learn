d3b00b2aa803ef983f4dce81c77f80f7be22cd0a
Jorge Campo <62282406+jorge-campo@users.noreply.github.com>
2021-09-03 14:33:58 +0200

    LRDOCS-9606 update POST and PUT scripts with 'here document' syntax

--- docs/dxp/latest/en/content-authoring-and-management/web-content/developer-guide/advanced-web-content-api/resources/liferay-m7b2.zip/curl/StructuredContent_POST_ToSite.sh
+++ docs/dxp/latest/en/content-authoring-and-management/web-content/developer-guide/advanced-web-content-api/resources/liferay-m7b2.zip/curl/StructuredContent_POST_ToSite.sh
@@ -1,6 +1,36 @@
 curl \
+	-H "accept: application/json" \
 	-H "Content-Type: application/json" \
-	-X POST \
+	-X 'POST' \
 	"http://localhost:8080/o/headless-delivery/v1.0/sites/${1}/structured-contents" \
-	-d "{\"contentFields\": [{\"contentFieldValue\": {\"data\": \"Foo\"}, \"name\": \"TextReference\"}, {\"contentFieldValue\": {\"image\": {\"description\": \"Foo alt-image description\", \"id\": \"${3}\"}}, \"name\": \"ImageReference\"}, {\"contentFieldValue\": {\"data\": \"2021-08-30T00:00:00Z\"}, \"dataType\": \"date\", \"name\": \"DateReference\"}, {\"contentFieldValue\": {\"data\": \"Foo\"}, \"name\": \"SingleSelectionReference\"}], \"contentStructureId\": \"${2}\", \"title\": \"Able Article\"}" \
-	-u "test@liferay.com:test"
+	-u "test@liferay.com:test" \
+	--data-binary @- << EOF
+		{
+			"contentFields" : [ {
+			"contentFieldValue" : {
+				"data" : "Foo"
+			},
+			"name" : "TextReference"
+			}, {
+			"contentFieldValue" : {
+				"image" : {
+					"description" : "Foo alt-image description",
+					"id" : "${3}"
+				}
+			},
+			"name" : "ImageReference"
+			}, {
+			"contentFieldValue" : {
+				"data" : "2021-08-30T00:00:00Z"
+			},
+			"name" : "DateReference"
+			}, {
+			"contentFieldValue" : {
+				"data" : "Foo"
+			},
+			"name" : "SingleSelectionReference"
+			} ],
+			"contentStructureId" : "${2}",
+			"title" : "Able Article"
+		}
+	EOF
--- docs/dxp/latest/en/content-authoring-and-management/web-content/developer-guide/advanced-web-content-api/resources/liferay-m7b2.zip/curl/StructuredContent_PUT_ById.sh
+++ docs/dxp/latest/en/content-authoring-and-management/web-content/developer-guide/advanced-web-content-api/resources/liferay-m7b2.zip/curl/StructuredContent_PUT_ById.sh
@@ -1,6 +1,35 @@
 curl \
 	-H "Content-Type: application/json" \
-	-X PUT \
+	-X 'PUT' \
 	"http://localhost:8080/o/headless-delivery/v1.0/structured-contents/${1}" \
-	-d "{\"contentFields\": [{\"contentFieldValue\": {\"data\": \"Goo\"}, \"name\": \"TextReference\"}, {\"contentFieldValue\": {\"image\": {\"description\": \"Goo alt-image description\", \"id\": \"${3}\"}}, \"name\": \"ImageReference\"}, {\"contentFieldValue\": {\"data\": \"2021-10-30T00:00:00Z\"}, \"dataType\": \"date\", \"name\": \"DateReference\"}, {\"contentFieldValue\": {\"data\": \"Goo\"}, \"name\": \"SingleSelectionReference\"}], \"contentStructureId\": \"${2}\", \"title\": \"Baker Article\"}" \
-    -u "test@liferay.com:test"
+    -u "test@liferay.com:test" \
+	--data-binary @- << EOF
+		{
+			"contentFields" : [ {
+			  "contentFieldValue" : {
+			  "data" : "Goo"
+			  },
+			  "name" : "TextReference"
+			}, {
+			  "contentFieldValue" : {
+				"image" : {
+					"description" : "Goo alt-image description",
+					"id" : "${3}"
+				}
+			},
+			"name" : "ImageReference"
+			}, {
+			"contentFieldValue" : {
+				"data" : "2021-10-30T00:00:00Z"
+			},
+			"name" : "DateReference"
+			}, {
+			"contentFieldValue" : {
+				"data" : "Goo"
+			},
+			"name" : "SingleSelectionReference"
+			} ],
+			"contentStructureId" : "${2}",
+			"title" : "Baker Article"
+		}
+	EOF
