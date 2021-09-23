99717fcd465887d0030e2755b95dc6cbf888bd10
jhinkey <james.hinkey@liferay.com>
2021-09-23 17:02:11 -0400

    LRDOCS-9649 return the un-parsed results

--- docs/dxp/latest/en/content-authoring-and-management/web-content/developer-guide/advanced-web-content-api/resources/liferay-m7b2.zip/curl/Documents_GET_FromSite.sh
+++ docs/dxp/latest/en/content-authoring-and-management/web-content/developer-guide/advanced-web-content-api/resources/liferay-m7b2.zip/curl/Documents_GET_FromSite.sh
@@ -1,5 +1,3 @@
-curl -s \
+curl \
 	"http://localhost:8080/o/headless-delivery/v1.0/sites/${1}/documents" \
-	-u "test@liferay.com:test" \
-	| awk '/"fileExtension" : "png"/{p=10} p>0 {print $0; p--}' \
-	| awk '/title/{print $3, "\n"}; /id/{print $3}'
+	-u "test@liferay.com:test"
