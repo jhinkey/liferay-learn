d6be4365b6b4d4cfc558a460ddb24671a46c9237
jhinkey <james.hinkey@liferay.com>
2021-12-01 13:43:52 -0500

    LRDOCS-9987 format with prettier

--- docs/dxp/latest/en/building-applications/objects/building-solutions-with-objects/using-object-data-with-react-charts/resources/liferay-x3j8.zip/liferay-x3j8/src/components/Chart.js
+++ docs/dxp/latest/en/building-applications/objects/building-solutions-with-objects/using-object-data-with-react-charts/resources/liferay-x3j8.zip/liferay-x3j8/src/components/Chart.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 
 import FusionCharts from 'fusioncharts';
 import Charts from 'fusioncharts/fusioncharts.charts';
@@ -10,70 +10,66 @@ import LiferayApi from '../common/services/liferay/api';
 ReactFC.fcRoot(FusionCharts, Charts, FusionTheme);
 
 const chartConfigs = {
-  type: 'column2d',
-  width: 600,
-  height: 400,
-  dataFormat: 'json',
-  dataSource: {/* see data tab */ }
+	type: 'column2d',
+	width: 600,
+	height: 400,
+	dataFormat: 'json',
+	dataSource: {
+		/* see data tab */
+	},
 };
 
 class Chart extends Component {
-  constructor(props) {
-    super(props);
-    this.state = chartConfigs;
-  }
+	constructor(props) {
+		super(props);
+		this.state = chartConfigs;
+	}
 
-  componentDidMount() {
-    setInterval(() => {
-      this.getUpdate()
-    }, 5000);
+	componentDidMount() {
+		setInterval(() => {
+			this.getUpdate();
+		}, 5000);
 
-    this.getUpdate();
-  }
+		this.getUpdate();
+	}
 
-  getChartData(data){
-    let chartData = [];
-    for (let i in data){
-        let obj = {};
-        obj.value = data[i].value;
-        obj.label = data[i].label;
-        chartData.push(obj);
-    }
-    return chartData;
-  }
+	getChartData(data) {
+		let chartData = [];
+		for (let i in data) {
+			let obj = {};
+			obj.value = data[i].value;
+			obj.label = data[i].label;
+			chartData.push(obj);
+		}
+		return chartData;
+	}
 
-  getUpdate(){
-    LiferayApi("o/c/dashboarddataobjects/")
-      .then((result) => {
-        //console.log(result.data.items);
+	getUpdate() {
+		LiferayApi('o/c/dashboarddataobjects/')
+			.then(result => {
+				//console.log(result.data.items);
 
-        this.setState({
-          
-          dataSource: {
-            "chart": {
-              "caption": "Impactful Data from Specific Areas",
-              "subCaption": "In OMDP = One Million Data Points",
-              "xAxisName": "Area",
-              "yAxisName": "Data Points (OMDP)",
-              "numberSuffix": "M",
-              "theme": "fusion",
-              "updateAnimDuration": "0.3"
-            },
-            "data": this.getChartData(result.data.items)
-          }
+				this.setState({
+					dataSource: {
+						chart: {
+							caption: 'Impactful Data from Specific Areas',
+							subCaption: 'In OMDP = One Million Data Points',
+							xAxisName: 'Area',
+							yAxisName: 'Data Points (OMDP)',
+							numberSuffix: 'M',
+							theme: 'fusion',
+							updateAnimDuration: '0.3',
+						},
+						data: this.getChartData(result.data.items),
+					},
+				});
+			})
+			.catch(console.log);
+	}
 
-        });
-
-      })
-      .catch(console.log)
-  }
-
-  render() {
-    return (
-      <ReactFC {...this.state} />
-    );
-  }
-
+	render() {
+		return <ReactFC {...this.state} />;
+	}
 }
 
 export default Chart;
